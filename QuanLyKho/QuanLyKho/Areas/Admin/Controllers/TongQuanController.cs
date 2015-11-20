79f495c630e7649bbba167d4db5dd1ed0fa213ed
cyprium <cyprium.black@gmail.com>
2015-11-21 00:27:23 +0700

    Sửa lại homecontroller ở admin thành tongquancontroller

--- QuanLyKho/QuanLyKho/Areas/Admin/Controllers/TongQuanController.cs
+++ QuanLyKho/QuanLyKho/Areas/Admin/Controllers/TongQuanController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace QuanLyKho.Areas.Admin.Controllers
 {
-    public class HomeController : Controller
+    public class TongQuanController : Controller
     {
         // GET: Admin/Home
         public ActionResult Index()
